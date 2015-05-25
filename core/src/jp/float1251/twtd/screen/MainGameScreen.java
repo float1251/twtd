76c0dc661eec1fc739b125469e54c5f0443bf960
takahiro.iwatani <taka.05022002@gmail.com>
2015-05-25 21:23:47 +0900

    load and render tiled

--- core/src/jp/float1251/twtd/screen/MainGameScreen.java
+++ core/src/jp/float1251/twtd/screen/MainGameScreen.java
@@ -3,6 +3,13 @@ package jp.float1251.twtd.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 import jp.float1251.twtd.TWTD;
 
@@ -11,25 +18,77 @@ import jp.float1251.twtd.TWTD;
  */
 public class MainGameScreen implements Screen {
     private final TWTD game;
+    private final FitViewport viewport;
+    private OrthogonalTiledMapRenderer renderer;
 
     public MainGameScreen(TWTD game) {
         this.game = game;
+        OrthographicCamera camera = new OrthographicCamera();
+        viewport = new FitViewport(960, 640, camera);
+        camera.setToOrtho(true, 960, 640);
     }
 
     @Override
     public void show() {
+        TiledMap loader = new TmxMapLoader().load("stage/stage1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(loader);
+        renderer.setView((OrthographicCamera) viewport.getCamera());
 
+        Gdx.input.setInputProcessor(new GestureDetector(new GestureDetector.GestureListener() {
+            @Override
+            public boolean touchDown(float x, float y, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean tap(float x, float y, int count, int button) {
+                game.setScreen(new MenuScreen(game));
+                return false;
+            }
+
+            @Override
+            public boolean longPress(float x, float y) {
+                return false;
+            }
+
+            @Override
+            public boolean fling(float velocityX, float velocityY, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean pan(float x, float y, float deltaX, float deltaY) {
+                return false;
+            }
+
+            @Override
+            public boolean panStop(float x, float y, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean zoom(float initialDistance, float distance) {
+                return false;
+            }
+
+            @Override
+            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+                return false;
+            }
+        }));
     }
 
     @Override
     public void render(float delta) {
+        viewport.getCamera().update();
         Gdx.gl20.glClearColor(0, 0, 1, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.render();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
