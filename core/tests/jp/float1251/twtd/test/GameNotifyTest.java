3caa28c880cccaba2bedcf6996bdfad2e2057863
float1251 <taka.05022002@gmail.com>
2015-08-30 15:15:07 +0900

    modify test

--- core/tests/jp/float1251/twtd/test/GameNotifyTest.java
+++ core/tests/jp/float1251/twtd/test/GameNotifyTest.java
@@ -1,10 +1,20 @@
 package jp.float1251.twtd.test;
 
+import com.badlogic.gdx.Game;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.invocation.MockitoMethod;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import jp.float1251.twtd.GameLog;
 import jp.float1251.twtd.listener.GameNotify;
 
 import static org.junit.Assert.assertEquals;
@@ -12,6 +22,8 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by t-iwatani on 2015/08/29.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(GameLog.class)
 public class GameNotifyTest {
 
     @Test
@@ -27,6 +39,12 @@ public class GameNotifyTest {
     public void sendMessage() throws InterruptedException {
         final GameNotify notify = new GameNotify();
         final CountDownLatch lock = new CountDownLatch(2);
+
+        // GameLog.dはGdx.app.logを読んでおり、testでは初期化されていないため、
+        // mockを作成し、何もさせないようにする
+        PowerMockito.mockStatic(GameLog.class);
+        PowerMockito.doNothing().when(GameLog.class);
+
         notify.addListener("test", new GameNotify.Runnable() {
             @Override
             public void run(Object... args) {
